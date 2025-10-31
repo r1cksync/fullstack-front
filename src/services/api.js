77cd8850dee66758f677beccd4bdc1de24d3f1da
r1cksync <152320439+r1cksync@users.noreply.github.com>
2025-10-31 22:59:46 +0530

    test

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
-  withCredentials: true,
 });
 
 // Add token to requests
